48237ff650add58e66bb5f28fe252bf8fd0e7001
bqanderson <bqanderson@me.com>
2016-07-01 17:09:46 -0500

    Add all functionality

--- app.js
+++ app.js
@@ -6,60 +6,27 @@ $(function() {
 
   var colorButton;
 
-if(colorButton=$('.color-button').data('color', 'red')){
-  $('.color-button').data('color', 'red').on('click', function(){
-    $('.container').append('<div class="color-cube red"></div>');
-    $('#red').text('Total red: ' + redCount++);
+  $('.color-button').on('click', function(){
+    colorButton = $(this).data('color');
+
+console.log(colorButton);
+
+    if(colorButton == 'red'){
+      $('.container').append('<div class="color-cube red">red</div>');
+      $('#red').text('Total red: ' + redCount++);
+
+    }else if(colorButton == 'yellow'){
+      $('.container').append('<div class="color-cube yellow">yellow</div>');
+      $('#yellow').text('Total yellow: ' + yellowCount++);
+
+    }else if(colorButton == 'green'){
+      $('.container').append('<div class="color-cube green">green</div>');
+      $('#green').text('Total green: ' + greenCount++);
+
+    }else if(colorButton == 'blue'){
+      $('.container').append('<div class="color-cube blue">blue</div>');
+      $('#blue').text('Total blue: ' + blueCount++);
+    }
+
   })
-
-}else if (colorButton=$('.color-button').data('color', 'yellow')) {
-  $('.color-button').data('color', 'yellow').on('click', function(){
-    $('.container').append('<div class="color-cube yellow"></div>');
-    $('#yellow').text('Total yellow: ' + yellowCount++);
-  })
-}
-
-
-  // $('.color-button').on('click', function(){
-  //   if(colorButton=$('.color-button').data('color', 'red')){
-  //     $('.container').append('<div class="color-cube red"></div>');
-  //     $('#red').text('Total red: ' + redCount++);
-
-    // }else if (colorButton=$('.color-button').data('color', 'yellow')) {
-    //   $('.container').append('<div class="color-cube yellow"></div>');
-    //   $('#yellow').text('Total yellow: ' + yellowCount++);
-    //
-    // }else if (colorButton=$('.color-button').data('color', 'green')) {
-    //   $('.container').append('<div class="color-cube green"></div>');
-    //   $('#green').text('Total green: ' + greenCount++);
-    //
-    // }else if (colorButton=$('.color-button').data('color', 'blue')) {
-    //   $('.container').append('<div class="color-cube blue"></div>');
-    //   $('#blue').text('Total blue: ' + blueCount++);
-    // }
-
-  // });
-
-  // $('.color-button').on('click', function(){
-  //   $('.container').append('<div class="color-cube yellow"></div>');
-  //   $('#yellow').text('Total red: ' + yellowCount++);
-  // });
-  //
-  // $('.color-button').on('click', function(){
-  //   $('.container').append('<div class="color-cube green"></div>');
-  //   $('#green').text('Total red: ' + greenCount++);
-  // });
-  //
-  // $('.color-button').on('click', function(){
-  //   $('.container').append('<div class="color-cube blue"></div>');
-  //   $('#blue').text('Total red: ' + blueCount++);
-  // });
-
-
-  // $.data('color').on('click', function(){
-  //   $('.container').append('<div class="color-cube red"></div>');
-  // });
-
-
-
 });
